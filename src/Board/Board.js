2613b7da027dfc6155ab1dc1fab4ae011e08059c
Alex <alglaze132@gmail.com>
2018-07-17 23:58:27 -0700

    add render sq function in board component

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import './Board.css';
 
+import '../Square/Square';
+
 class Board extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      board: ['', '', '', '', '', '', '', '', '']
-    };
   }
+
+  renderSquare(i) {
+    return <Square />;
+  }
+
   render() {
     return (
-      <div className="board">
-        {this.state.board.map((cell, index) => {
-          return <div key={index} className="square" />;
-        })}
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
       </div>
     );
   }
