6ebd54550014c1ed153e725ff1363d3feec7b64c
Alex <alglaze132@gmail.com>
2018-07-18 19:44:25 -0700

    board file comments

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -4,6 +4,7 @@ import './Board.css';
 import Square from '../Square/Square';
 
 class Board extends Component {
+  //helper fn that renders squares
   renderSquare = i => {
     return (
       <Square
@@ -14,21 +15,22 @@ class Board extends Component {
     );
   };
 
+  //helper fn that we call directly from render to create our board.
   createBoard = () => {
     let board = [];
     let index = 0;
     let rowIndex = 0;
-    //outer loop to create parent
+    //outer loop to create board
     for (let i = 0; i < 3; i++) {
       rowIndex++;
       let row = [];
 
-      //inner loop to create children
+      //inner loop to create rows
       for (let j = 0; j < 3; j++) {
         row.push(this.renderSquare(index));
         index++;
       }
-
+      //add the newly created row to our board
       board.push(
         <div key={rowIndex} className="board-row">
           {row}
